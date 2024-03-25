417e32bc5ee608140a89fb7f9fd2c387964b3bfd
deep bhalani <121185097+Deepbhalani@users.noreply.github.com>
2024-03-25 16:16:33 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.get('/', async(req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'home.html'));
+});
+app.listen(5500, () => {
+    console.log("Server successfully running on port 5500");
+});
